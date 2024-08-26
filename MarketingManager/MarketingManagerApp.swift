451ba653856bd48730c421491e04af24e7107b76
Radu Paun <paunraduadrian@gmail.com>
2024-08-26 15:11:32 +0300

    Initial Commit

---
+++ MarketingManager/MarketingManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketingManagerApp.swift
+//  MarketingManager
+//
+//  Created by Radu Paun on 26.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MarketingManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
